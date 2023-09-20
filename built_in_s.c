9cac67c4756c232a69ec903962540be1a8a61984
Achraf <touilashraf6@gmail.com>
2023-09-20 13:36:25 -0700

    built in task

---
+++ built_in_s.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * cd_cmd - Change the current working directory
+ * @input: The array of command arguments
+ */
+void  cd_cmd(char **input)
+{
+	char *current_dir = NULL;
+	char *cur_pwd = NULL;
+
+	current_dir = getcwd(NULL, 0);
+
+	if (!current_dir)
+	{
+		perror("getcwd() error");
+		return;
+	}
+	if (!input[1])
+	{
+		if (chdir(bring_path("HOME")) != 0)
+		{
+			perror("chdir() error");
+		}
+	}
+	else
+	{
+		if (chdir(input[1]) != 0)
+		{
+			perror("chdir() error");
+		}
+	}
+	cur_pwd = getcwd(NULL, 0);
+	if (!cur_pwd)
+	{
+		perror("getcwd() error");
+		return;
+	}
+	free(cur_pwd);
+}
